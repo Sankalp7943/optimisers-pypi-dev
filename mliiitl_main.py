714f958aa3d23921398deef0f9160149ab009a74
Sankalp Sharma <sharmasankalp@outlook.com>
2021-01-23 15:53:43 +0530

    created main package file

---
+++ mliiitl_main.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib as plt
+import keras
+import tensorflow as tf
+
+class mliiitl:
+    def __init__(self, x_train, y_train, x_test, y_test, 
+                 model, loss, epoch, batch_size):
+        self._x_train = x_train
+        self._y_train
+        self._x_test=
+        self._y_test=
+        self._model=model
+        self._loss =
+        self._epoch
+        self._batch_size
+        
+        
+
