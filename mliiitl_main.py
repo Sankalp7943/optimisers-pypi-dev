a8666404b34f492f7623c066efd08bc70aec6309
Sankalp Sharma <sharmasankalp@outlook.com>
2021-01-24 19:15:11 +0530

    added splicing factor

--- mliiitl_main.py
+++ mliiitl_main.py
@@ -58,23 +58,26 @@ class mliiitl:
         model.save('temp_model')
         return 'temp_model'
     
-    def splice_dataset_randomly(x_train,y_train):
+    def splice_dataset_randomly(x_train, y_train, factor):
         '''
         splices 1/8th data randomly for training
         '''
+        if factor < 1:
+            factor = 1
+            print("Factor cannot be less than 1, defaulted to value 1")
         number_of_rows = x_train.shape[0]
-        random_indices = np.random.choice(number_of_rows, size=number_of_rows//8, replace=False)
+        random_indices = np.random.choice(number_of_rows, size=number_of_rows//factor, replace=False)
         spliced_x_train = x_train[random_indices, :]
         spliced_y_train = y_train[random_indices, :]
 
         return spliced_x_train,spliced_y_train
     
-    def test_performance(self, plots = False, save = False):
+    def test_performance(self, plots = False, save = False, factor = 8):
         '''
 
         '''
         temp = save_model_instance(self._model)
-        spliced_x_train, spliced_y_train = splice_dataset_randomly(self._x_train, self._y_train)
+        spliced_x_train, spliced_y_train = splice_dataset_randomly(self._x_train, self._y_train, factor)
 
         model_sgd = tf.keras.models.load_model('temp_model')
         model_rmsprop = tf.keras.models.load_model('temp_model')
